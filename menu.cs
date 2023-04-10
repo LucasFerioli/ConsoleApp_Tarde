e10dde8e1bbf41f551b830f92b8ebac4073b89eb
LucasFerioli <127348060+LucasFerioli@users.noreply.github.com>
2023-04-10 20:39:36 -0300

    Create menu.cs

---
+++ menu.cs
@@ -0,0 +1,12 @@
+<nav>
+    <ul>
+        <li><a href="/Produtos/Lista">Produtos</a></li>
+        <li><a href="/Produtos/Cadastro">Cadastrar Produto</a></
+<nav>
+    <ul>
+        <li><a href="/Produtos/Lista">Produtos</a></li>
+        <li><a href="/Produtos/Cadastro">Cadastrar Produto</a></li>
+        <li><a href="/Fornecedores/Lista">Fornecedores</a></li>
+        <li><a href="/Fornecedores/Cadastro">Cadastrar Fornecedor</a></li>
+    </ul>
+</nav>
